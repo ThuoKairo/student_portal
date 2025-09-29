e15c243c0dcd9f6bfe241f5757f58ca926183c0b
ThuoKairo <kairo.thuo@strathmore.edu>
2025-09-29 20:35:07 +0300

    Your commit message here

--- students/views.py
+++ students/views.py
@@ -3,7 +3,3 @@ from django.shortcuts import render
 #Home view for portal
 def home(request):
     return render(request, 'students/home.html')
-
-#About view for portal 
-def about(request):
-    return render(request, 'students/about.html')
